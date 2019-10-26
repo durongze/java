5e0079826161ef5c405938fbfafe12480d3b210d
durongze@qq.com <durongze@qq.com>
2019-10-26 21:10:12 +0800

    ndk 编译命令

--- callc/build.bat
+++ callc/build.bat
@@ -1,5 +1,6 @@
 javac -d . callc.java
 javah -jni com.durongze.jni.CallC
+ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=Application.mk APP_BUILD_SCRIPT=Android.mk
 mkdir dyzbuild
 pushd dyzbuild
 cmake -G "Visual Studio 14 2015 Win64" ..
