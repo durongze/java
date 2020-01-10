271682a5eac648c9e72febc311879c3e712417c0
durongze@qq.com <durongze@qq.com>
2020-01-10 21:38:58 +0800

    java 版实现单机效果

--- test/Web.java
+++ test/Web.java
@@ -1,17 +1,44 @@
 package com.durongze;
 
+import java.io.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.ie.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Web 
 {
+    WebDriver iebw = null;
+    String url = "www.baidu.com";
+    String btn = "su";
+    
+    public Web()
+    {
+        iebw = new InternetExplorerDriver();
+    }
+    
     public void SwitchToSpecFrame()
     {
         
     }
     
+    public void ExecJavaScript(WebElement element)
+    {
+        JavascriptExecutor js = (JavascriptExecutor)iebw;
+        try {
+            Thread.sleep(5000);
+            js.executeScript("arguments[0].click();", element);
+        } catch (Exception e) {
+            System.out.println("click fail");
+        }
+    }
+    
     public void GetWebPage()
     {
-        System.out.println("web\n");
+        iebw.get(url);
+        WebElement btnElement = iebw.findElement(By.id(btn));
+        
         return ;
     }
     
