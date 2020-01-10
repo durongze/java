27dacf0e53c6bd0858e2761d6e15b700f0250c11
durongze@qq.com <durongze@qq.com>
2020-01-10 21:09:29 +0800

    c# web selenium

---
+++ c#/web/Web.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+class Web{
+    IWebDriver iebw;
+    string url = "www.baidu.com";
+    string btn = "su";
+    string jsZoom = "document.body.style.zoom='1'";
+    public Web() {
+        iebw = new InternetExplorerDriver();
+        // iebw = new ChromeDriver();
+    }
+    ~Web()
+    {
+        iebw.Close();
+    }
+    void GetWebPage() {
+        ExecJavaScript(jsZoom);
+        iebw.Navigate().GoToUrl(url);
+        IWebElement element = iebw.FindElement(By.Id(btn));
+        ExecJavaScript(element);
+    }
+
+    void ExecJavaScript(IWebElement element)
+    {
+        IJavaScriptExecutor js = (IJavaScriptExecutor)iebw;
+        Thread.Sleep(5000);
+        js.ExecuteScript("arguments[0].click();", element);
+    }
+
+    void ExecJavaScript(string jsCmd)
+    {
+        IJavaScriptExecutor js = (IJavaScriptExecutor)iebw;
+        Thread.Sleep(5000);
+        js.ExecuteScript(jsCmd);
+    }
+
+    public static void Main(string[] args) {
+        Console.WriteLine("Main");
+        Web wc = new Web();
+        wc.GetWebPage();
+    }
+}
---
+++ c#/web/build_web.bat
@@ -0,0 +1,9 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+mkdir dyzbuild
+pushd dyzbuild
+cmake ..
+MSBuild Web.csproj /p:Configuration=Release /p:Platform=x86
+popd
+
+for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
+pause
