79d2f6dd7f7c1a33df9738d6dbf78636fa0165e8
durongze@qq.com <durongze@qq.com>
2020-02-12 09:50:32 +0800

    使用google 浏览器

--- test/Web.java
+++ test/Web.java
@@ -2,6 +2,7 @@ package com.durongze;
 
 import java.io.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.ie.*;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.By;
@@ -10,12 +11,21 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class Web 
 {
     WebDriver iebw = null;
-    String url = "www.baidu.com";
+    String url = "http://www.baidu.com";
+	String kw = "kw";
+	String kw = "kw";
     String btn = "su";
-    
+    String searchKw = "duyongze";
     public Web()
     {
-        iebw = new InternetExplorerDriver();
+        // iebw = new InternetExplorerDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-infobars");
+		options.setBinary("D:/Program Files/Google/Chrome/Application/chrome.exe");
+		// System.setProperty("webdriver.chrome.driver", "C:/Windows");
+		iebw = new ChromeDriver(options);
+		iebw.manage().window().maximize();
+		
     }
     
     public void SwitchToSpecFrame()
@@ -37,8 +47,11 @@ public class Web
     public void GetWebPage()
     {
         iebw.get(url);
+        WebElement kwElement = iebw.findElement(By.id(kw));
+		kwElement.sendKeys(searchKw);
+		kwElement.sendKeys(Keys.ENTER);
         WebElement btnElement = iebw.findElement(By.id(btn));
-        
+		ExecJavaScript(btnElement);
         return ;
     }
     
