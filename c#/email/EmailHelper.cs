9ecff321a08286906c99c74f9fba41280bc58f3c
durongze@qq.com <durongze@qq.com>
2020-03-09 23:58:46 +0800

    OutLook

---
+++ c#/email/EmailHelper.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Office.Interop.Outlook;
+
+class EmailHelper {
+    String m_excelName;
+    String m_exportPath;
+
+    public EmailHelper() {
+
+    }
+
+    // 在属性中修改为控制台程序，才能看到输出
+    public static void Main(string[] args){
+
+    }
+}
---
+++ c#/email/build_email.bat
@@ -0,0 +1,13 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+mkdir lib
+rem tlbimp excel.exe /out:lib\excel.dll 
+mkdir dyzbuild
+pushd dyzbuild
+cmake ..
+MSBuild EmailHelper.csproj /p:Configuration=Release /p:Platform=x86
+popd
+
+for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
+tasklist  | findstr /i "EmailHelper"
+taskkill /f /im EmailHelper.exe
+pause
