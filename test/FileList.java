e72447a60df47b49e5c246d5d543bf5fe214bf23
durongze@qq.com <durongze@qq.com>
2020-03-09 18:53:16 +0800

    File List

--- test/FileList.java
+++ test/FileList.java
@@ -1,6 +1,8 @@
 package com.durongze;
 
 import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class FileList{
     public static void ShowFileList(String[] fileList)
@@ -27,6 +29,42 @@ public class FileList{
         }
     }
     
+    public byte[] ReadBinary(File f) throws Exception 
+    {
+        InputStream in = new FileInputStream(f);
+        byte[] data = new byte[(int)f.length()];
+        try {
+            System.out.println("f size:" + f.length());
+            in.read(data);
+            OutputStream out = new FileOutputStream(new File("d:\\xx.bin"));
+            out.write(data);
+            out.close();
+        } catch (Exception e) {
+            // System.out.println("dir:");
+        }
+        return data;
+    }
+    public void ProcAllFile(File[] fileList) throws Exception 
+    {
+        System.out.println("ShowFilePath");
+        for (File f:fileList) {
+            if (!f.exists()) {
+                continue;
+            }
+            if (f.isFile()) {
+                // System.out.println("file:");
+                String fext = f.getAbsolutePath();
+                if (fext.contains(".txt")) {
+                    byte[] ctx = ReadBinary(f);
+                    File fileExt = new File(fext.replace(".txt", ".bin"));
+                    System.out.println(fileExt.getAbsolutePath());
+                }
+            } else if(f.isDirectory()) {
+                // System.out.println("dir:");
+            }
+            
+        }
+    }
     // inputStream和OutputStream的子类都是字节流
     // 主要处理音频、图片、歌曲、字节流（1字节处理）
     // Reader和Writer的子类都是字符流
@@ -70,11 +108,13 @@ public class FileList{
     //      - PipedReader
     //      - StringReader
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File file = new File("D:\\");
-        String[] fileNameList = file.list();
-        ShowFileList(fileNameList);
+        // String[] fileNameList = file.list();
+        // ShowFileList(fileNameList);
         File[] fileList = file.listFiles();
-        ShowFilePath(fileList);
+        // ShowFilePath(fileList);
+        FileList fl = new FileList();
+        fl.ProcAllFile(fileList);
     }
 }
--- test/build_file.bat
+++ test/build_file.bat
@@ -3,13 +3,13 @@ set proj_dir=%cd%
 set all_jar=%proj_dir%\lib\selenium-server-standalone-3.141.59.jar
 
 set PATH=%PATH%;%JAVA_HOME%\bin;
-javac -classpath %all_jar% %proj_name%.java
+javac  -encoding utf-8 -d . -classpath %all_jar% %proj_name%.java
 
 set CLASSPATH=%CLASSPATH%;%all_jar%;
 
-mkdir com\durongze\
+mkdir com\durongze
 
-move %proj_name%.class com/durongze/
+move %proj_name%.class com\durongze
 
 java com.durongze.%proj_name%
  
