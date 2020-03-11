da29205fe3816d61315c08ffdf359e8c67530cf1
durongze@qq.com <durongze@qq.com>
2020-03-11 12:55:35 +0800

    java email

---
+++ test/EmailHelper.java
@@ -0,0 +1,95 @@
+package com.durongze;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.property.BodyType;
+import microsoft.exchange.webservices.data.core.enumeration.property.WellKnownFolderName;
+import microsoft.exchange.webservices.data.core.service.folder.Folder;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.core.service.item.Item;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+import microsoft.exchange.webservices.data.search.FindItemsResults;
+import microsoft.exchange.webservices.data.search.ItemView;
+
+public class EmailHelper {
+
+    private String mailServer = "imap.qq.com";
+    private String user = "durongze@qq.com";
+    private String password = ".adgjmptw0.adgjmptw0";
+
+    public EmailHelper(){
+    }
+    public EmailHelper(String mailServer, String user, String password){
+        this.mailServer = mailServer;
+        this.user = user;
+        this.password = password;
+    }
+
+    /**
+     * 发送带附件的mail
+     */
+    public void doSend(String subject, String[] to, String[] cc, String bodyText, String[] attachmentPath) throws Exception {
+        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+        ExchangeCredentials credentials = new WebCredentials(user, password);
+        service.setCredentials(credentials);
+        try {
+            service.setUrl(new URI(mailServer));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        EmailMessage msg = new EmailMessage(service);
+        msg.setSubject(subject);
+        MessageBody body = MessageBody.getMessageBodyFromText(bodyText);
+        body.setBodyType(BodyType.HTML);
+        msg.setBody(body);
+        for (String s : to) {
+            msg.getToRecipients().add(s);
+        }
+        if (cc != null) {
+            for (String s : cc) {
+                msg.getCcRecipients().add(s);
+            }
+        }
+        if (attachmentPath != null && attachmentPath.length > 0) {
+            for (int a = 0; a < attachmentPath.length; a++) {
+                msg.getAttachments().addFileAttachment(attachmentPath[a]);
+            }
+
+        }
+        msg.send();
+    }
+
+    /**
+     * 发送不带附件的mail
+     */
+    public void send(String subject, String[] to, String[] cc, String bodyText) throws Exception {
+        doSend(subject, to, cc, bodyText, null);
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        EmailHelper eh = new EmailHelper();
+        String subject = "subject";
+        String[] to = new String[]{"durongze@qq.com"};
+        String cc = "duyongzeyx@qq.com";
+        String content = "ctx";
+        eh.send(subject, to, to, content);
+    }
+}
---
+++ test/build_email.bat
@@ -0,0 +1,44 @@
+set proj_name=EmailHelper
+set proj_dir=%cd%
+set all_jar=%proj_dir%\lib\selenium-server-standalone-3.141.59.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-examples-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-excelant-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-ooxml-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-ooxml-schemas-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\poi-scratchpad-4.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\selenium-server-standalone-3.141.59.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\activation-1.1.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\commons-codec-1.13.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\commons-collections4-4.4.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\commons-compress-1.19.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\commons-logging-1.2.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\commons-math3-3.6.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\jaxb-api-2.3.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\jaxb-core-2.3.0.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\jaxb-impl-2.3.2.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\junit-4.12.jar
+set all_jar=%all_jar%;%proj_dir%\lib\lib\log4j-1.2.17.jar
+set all_jar=%all_jar%;%proj_dir%\lib\ooxml-lib\curvesapi-1.06.jar
+set all_jar=%all_jar%;%proj_dir%\lib\ooxml-lib\xmlbeans-3.1.0.jar
+set all_jar=%all_jar%;%proj_dir%\lib\commons-io-2.4.jar
+set all_jar=%all_jar%;%proj_dir%\lib\commons-logging-1.2.jar
+set all_jar=%all_jar%;%proj_dir%\lib\httpclient-4.4.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\httpcore-4.4.1.jar
+set all_jar=%all_jar%;%proj_dir%\lib\joda-time-2.10.jar
+set all_jar=%all_jar%;%proj_dir%\lib\org.apache.commons.codec_1.10.0.v20180409-1845.jar
+set all_jar=%all_jar%;%proj_dir%\lib\org.apache.commons.lang3_3.1.0.v201403281430.jar
+set all_jar=%all_jar%;%proj_dir%\lib\ews-java-api.jar
+
+set PATH=%PATH%;%JAVA_HOME%\bin;
+javac -encoding utf-8 -d . -classpath %all_jar% %proj_name%.java
+
+set CLASSPATH=%CLASSPATH%;%all_jar%;
+
+mkdir com\durongze\
+
+move %proj_name%.class com/durongze/
+
+java com.durongze.%proj_name%
+ 
+pause
