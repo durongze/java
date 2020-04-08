3af641a3d0d5abf5e63e3427554895bbf7fd49d6
durongze@qq.com <durongze@qq.com>
2020-04-08 23:37:07 +0800

    vs 依然不能跑，但是控制台可以跑

--- CppCallJni.cpp
+++ CppCallJni.cpp
@@ -2,43 +2,65 @@
 #include <Windows.h>
 
 #define JAVA_HOME "D:\\Program Files\\Java\\jre1.8.0_60"
-
 #define JVM_DLL JAVA_HOME "\\bin\\server\\" "jvm.dll"
-typedef int (*JNICreateJavaVM)(JavaVM **pvm, void **penv, void *args);
-int main() {
-	JavaVM *pvm = NULL;
-	JNIEnv *penv = NULL;
-	JavaVMInitArgs args;
-	JavaVMOption options[1] = {"-Djava.class.path=.;D:\\Program Files\\Java\\jre1.8.0_60\\lib\\dt.jar;D:\\Program Files\\Java\\jre1.8.0_60\\lib\\tools.jar"};
-	args.options = options;
-	args.nOptions = 1;
-	args.version = JNI_VERSION_1_6;
-	args.ignoreUnrecognized = JNI_TRUE;
-	char *jvmDll = JVM_DLL;
-	HMODULE hmod = LoadLibraryA("D:\\Program Files\\Java\\jre1.8.0_60\\bin\\server\\jvm.dll");
-	JNICreateJavaVM pJNICreateJavaVM = (JNICreateJavaVM)GetProcAddress(hmod, "JNI_CreateJavaVM");
-	int status = pJNICreateJavaVM(&pvm, (void**)&penv, (void**)&args);
-	if (status == JNI_ERR) {
-		return 0;
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif
+
+	typedef int(*JNICreateJavaVM)(JavaVM **pvm, void **penv, void *args);
+	typedef int (*JNIGetCreatedJavaVMs)(JavaVM **vmBuf, jsize bufLen, jsize *nVMs);
+	int main() {
+		int status = 0;
+		JavaVM *pvm = NULL;
+		JNIEnv *penv = NULL;
+		JavaVMInitArgs args;
+		JavaVMOption options[4];
+		options[0].optionString = "-Djava.compiler=NONE";
+		options[1] = { "-Djava.class.path=.;D:\\Program Files\\Java\\jre1.8.0_60\\lib\\dt.jar;D:\\Program Files\\Java\\jre1.8.0_60\\lib\\tools.jar", NULL };
+		options[2].optionString = "-verbose:NONE";
+		options[3].optionString = "-XX:+CreateMinidumpOnCrash";
+		args.options = options;
+		args.nOptions = 4;
+		args.version = JNI_VERSION_1_8;
+		args.ignoreUnrecognized = JNI_TRUE;
+		char *jvmDll = JVM_DLL;
+		HMODULE hmod = LoadLibraryA("D:\\Program Files\\Java\\jre1.8.0_60\\bin\\server\\jvm.dll");
+		JNICreateJavaVM pJNICreateJavaVM = (JNICreateJavaVM)GetProcAddress(hmod, "JNI_CreateJavaVM");
+		JNIGetCreatedJavaVMs pJNIGetCreateJavaVMs = (JNIGetCreatedJavaVMs)GetProcAddress(hmod, "JNI_GetCreatedJavaVMs");
+		JavaVM *pvms[32] = { 0 };
+		jsize pvmsnum = 0;
+		status = pJNIGetCreateJavaVMs(pvms, sizeof(pvms), &pvmsnum);
+		status = pJNICreateJavaVM(&pvm, (void**)&penv, (void*)&args);
+		if (status == JNI_ERR) {
+			return 0;
+		}
+		int num = 2;
+		jclass intArrCls = penv->FindClass("java/lang/String");
+		jobjectArray names = penv->NewObjectArray(num, intArrCls, NULL);
+		jintArray ages = penv->NewIntArray(num);
+		jfloatArray heights = penv->NewFloatArray(num);
+
+		for (int idx = 0; idx < num; ++idx) {
+			const char *pns = "xxx";
+			jstring ns = penv->NewStringUTF(pns);
+			jint intArr[1] = { idx };
+			jfloat floatArr[1] = { idx };
+			penv->SetIntArrayRegion(ages, idx, 1, intArr);
+			penv->SetFloatArrayRegion(heights, idx, 1, floatArr);
+			penv->SetObjectArrayElement(names, idx, ns);
+			penv->ReleaseStringUTFChars(ns, 0);
+		}
+
+		jobjectArray res = (jobjectArray)Java_com_durongze_jni_CallC_CInterface(penv, NULL, names, ages, heights, num);
+
+		pvm->DestroyJavaVM();
 	}
-	int num = 2;
-	jclass intArrCls = penv->FindClass("java/lang/String");
-	jobjectArray names = penv->NewObjectArray(num, intArrCls, NULL);
-	jintArray ages = penv->NewIntArray(num);
-	jfloatArray heights = penv->NewFloatArray(num);
 
-	for (int idx = 0; idx < num; ++idx) {
-		const char *pns = "xxx"; 
-		jstring ns = penv->NewStringUTF(pns);
-		jint intArr[1] = { idx };
-		jfloat floatArr[1] = { idx };
-		penv->SetIntArrayRegion(ages, idx, 1, intArr);
-		penv->SetFloatArrayRegion(heights, idx, 1, floatArr);
-		penv->SetObjectArrayElement(names, idx, ns);
-		penv->ReleaseStringUTFChars(ns, 0);
-	}
-
-	jobjectArray res = (jobjectArray)Java_com_durongze_jni_CallC_CInterface(penv, NULL, names, ages, heights, num);
-
-	pvm->DestroyJavaVM();
+#ifdef __cplusplus
+#if __cplusplus
 }
+#endif
+#endif
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_60
 set PATH=%PATH%;%JAVA_HOME%\bin;E:\Android\sdk\ndk-bundle\android-ndk-r20
-
+java -version
 rem ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=Application.mk APP_BUILD_SCRIPT=Android.mk
 
 mkdir dyzbuild
@@ -8,4 +8,7 @@ pushd dyzbuild
 cmake -G "Visual Studio 14 2015 Win64" ..
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 msbuild CppCallJni.vcxproj
+Debug\CppCallJni.exe
 popd
+
+pause
