f4e4dffb17b91b24dd47835eff0d38ae1570974a
durongze@qq.com <durongze@qq.com>
2020-11-03 22:42:22 +0800

    将已经存在的/MD、/MDd替换为/MT、/MTd java.lang.UnsatisfiedLinkError Can't find dependent libraries，

--- callc/build.bat
+++ callc/build.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_60
+set JAVA_HOME=D:\Program Files\Java\jdk-12.0.2
 set PATH=%PATH%;%JAVA_HOME%\bin;
 set PATH=%PATH%;E:\Android\sdk\ndk-bundle\android-ndk-r20;D:\Android\ndk\android-ndk-r19c
 javac -encoding utf-8 -d . callc.java
@@ -7,15 +7,20 @@ rem javah -jni com.durongze.jni.CallC
 javac -encoding utf-8 -h . callc.java
 
 rem ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=Application.mk APP_BUILD_SCRIPT=Android.mk
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 mkdir dyzbuild
 pushd dyzbuild
-cmake -G "Visual Studio 14 2015 Win64" ..
+rem cmake -G "Visual Studio 14 2015 Win64" ..
+rem Win32 x64
+del * /s /q
+cmake -G "Visual Studio 16 2019" -A x64 ..
 msbuild CLibrary.vcxproj
 popd
 
 rem del "%JAVA_HOME%\bin\CLibrary.dll"
 for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
+del "%JAVA_HOME%\bin\libCLibrary.dll"
 copy ".\CLibrary.dll" "%JAVA_HOME%\bin\CLibrary.dll"
 java com.durongze.jni.CallC
 pause
