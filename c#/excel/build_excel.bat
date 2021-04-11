936c2f19f6a7ff7b719d092f0f93fb150feebf8f
durongze <durongze@qq.com>
2021-04-11 21:04:28 +0800

    vs2019

--- c#/excel/build_excel.bat
+++ c#/excel/build_excel.bat
@@ -1,10 +1,13 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+
 mkdir lib
 rem tlbimp excel.exe /out:lib\excel.dll 
 mkdir dyzbuild
 pushd dyzbuild
-cmake ..
-MSBuild WebExcel.csproj /p:Configuration=Release /p:Platform=x86
+rem cmake ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
+MSBuild WebExcel.csproj /p:Configuration=Release /p:Platform=x64
 popd
 
 for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
--- c#/web/build_web.bat
+++ c#/web/build_web.bat
@@ -1,8 +1,11 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+
 mkdir dyzbuild
 pushd dyzbuild
-cmake ..
-MSBuild Web.csproj /p:Configuration=Release /p:Platform=x86
+rem cmake ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
+MSBuild Web.csproj /p:Configuration=Release /p:Platform=x64
 popd
 
 for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
