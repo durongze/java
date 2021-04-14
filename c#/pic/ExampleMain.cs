5158f74b155a10e54f378c1025f237794b659541
durongze <durongze@qq.com>
2021-04-14 22:24:26 +0800

    动态字

--- c#/pic/ExampleMain.cs
+++ c#/pic/ExampleMain.cs
@@ -2,42 +2,101 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Collections.Generic;
 using Gif.Components;
 
 namespace Example
 {
 	class ExampleMain
 	{
-		[STAThread]
-		static void Main(string[] args)
+		static void DrawYellowBox(int bx, int by, Graphics g)
 		{
-			/* create Gif */
-			//you should replace filepath
-			String[] imageFilePaths = new String[] { "..\\..\\Res\\01.png", "..\\..\\Res\\02.png" }; //, "..\\..\\Res\\03.png" }; 
-			String outputFilePath = "..\\..\\Res\\test.gif";
+			SolidBrush yellowBrush = new SolidBrush(Color.FromArgb(255, 255, 0));
+			SolidBrush greenBrush = new SolidBrush(Color.FromArgb(0, 128, 0));
+			SolidBrush blackBrush = new SolidBrush(Color.Black);
+			Pen borderPen = new Pen(Color.Black);
+			Pen yellowPen = new Pen(Color.Yellow, 2); 
+													  
+			g.FillRectangle(yellowBrush, bx, by, 30, 30);
+			g.FillRectangle(greenBrush, bx + 3, by + 3, 24, 24);
+	
+			g.DrawLine(borderPen, bx + 3, by + 5, bx + 24, by + 26);
+			g.DrawLine(borderPen, bx + 5, by + 3, bx + 26, by + 24);
+			g.DrawLine(yellowPen, bx + 3, by + 3, bx + 26, by + 26);
+			g.DrawLine(borderPen, bx + 24, by + 3, bx + 4, by + 23);
+			g.DrawLine(borderPen, bx + 26, by + 5, bx + 5, by + 26);
+			g.DrawLine(yellowPen, bx + 26, by + 3, bx + 3, by + 26);
+
+			g.DrawRectangle(borderPen, bx, by, 29, 29);
+			g.DrawRectangle(borderPen, bx + 3, by + 3, 23, 23);
+		}
+
+		static int CalcCoordX(int width, int word, int idx)
+		{
+			return idx % width * word;
+		}
+		static int CalcCoordY(int width, int word, int idx)
+		{
+			return idx / width * word;
+		}
+		static void GenImages(String str, String imageFile, List<Image> images)
+		{
+			int x, y;
+			Image img;
+			img = Image.FromFile(imageFile);
+			Graphics g = Graphics.FromImage(img);
+			for (int i = 0; i < str.Length; ++i) 
+			{
+				x = CalcCoordX(6, 40, i);
+				y = CalcCoordY(6, 40, i);
+				DrawYellowBox(x, y, g);
+				images.Add((Image)img.Clone());
+				g = Graphics.FromImage(img);
+			}
+		}
+
+		static void LoadImages(String[] imageFiles, List<Image> images)
+		{
+			for (int i = 0; i < imageFiles.Length; ++i)
+			{
+				images.Add(Image.FromFile(imageFiles[i]));
+			}
+		}
+		static void EncGifFile(List<Image> images, String outputFile)
+		{
+			MemoryStream memStream = new MemoryStream();
 			AnimatedGifEncoder e = new AnimatedGifEncoder();
-            
-            // read file as memorystream
-		    byte[] fileBytes = File.ReadAllBytes(outputFilePath);
-            MemoryStream memStream = new MemoryStream(fileBytes);
-            e.Start(memStream);
+			e.Start(memStream);
 			e.SetDelay(500);
 			//-1:no repeat,0:always repeat
 			e.SetRepeat(0);
-			for (int i = 0, count = imageFilePaths.Length; i < count; i++ ) 
+			for (int i = 0, count = images.Count; i < count; i++)
 			{
-				e.AddFrame( Image.FromFile( imageFilePaths[i] ) );
+				e.AddFrame(images[i]);
 			}
 			e.Finish();
-			/* extract Gif */
-			string outputPath = "c:\\";
+			FileStream fs = new FileStream(outputFile, FileMode.OpenOrCreate);
+			BinaryWriter w = new BinaryWriter(fs);
+			w.Write(memStream.ToArray());
+			fs.Close();
+			memStream.Close();
+		}
+		static void DecGifFile(string gifFile, string outputPath)
+		{
 			GifDecoder gifDecoder = new GifDecoder();
-			gifDecoder.Read( "c:\\test.gif" );
-			for ( int i = 0, count = gifDecoder.GetFrameCount(); i < count; i++ ) 
+			gifDecoder.Read(gifFile);
+			for (int i = 0, count = gifDecoder.GetFrameCount(); i < count; i++)
 			{
-				Image frame = gifDecoder.GetFrame( i );  // frame i
-				frame.Save( outputPath + Guid.NewGuid().ToString() + ".png", ImageFormat.Png );
+				Image frame = gifDecoder.GetFrame(i);  // frame i
+				frame.Save(outputPath + Guid.NewGuid().ToString() + ".png", ImageFormat.Png);
 			}
+		}
+		[STAThread]
+		static void Main(string[] args)
+		{
+			List<Image> images = new List<Image>();
+			GenImages("durongze", "res/00.png", images);
+			EncGifFile(images, "res/out.gif");
 		}
 	}
 }
