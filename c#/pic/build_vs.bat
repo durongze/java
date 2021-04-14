7a685382c8c01ad11372d0d1d2884642a3aae927
durongze <durongze@qq.com>
2021-04-14 21:24:21 +0800

    pic

---
+++ c#/pic/build_vs.bat
@@ -0,0 +1,14 @@
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+
+mkdir dyzbuild
+pushd dyzbuild
+rem cmake ..
+cmake -G "Visual Studio 16 2019" -A x64 ..
+MSBuild libgif.csproj /p:Configuration=Release /p:Platform=x64
+MSBuild gifApp.csproj /p:Configuration=Release /p:Platform=x64
+popd
+
+for /f %%i in ('dir /s /b "*.dll"') do (copy %%i .\)
+for /f %%i in ('dir /s /b "*.exe"') do (copy %%i .\)
+pause
