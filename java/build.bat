b7244e73f3f375bfa8bfd99520ccc4a8ee750386
durongze <durongze@qq.com>
2026-02-20 15:15:26 +0800

    fix java_home

--- java/build.bat
+++ java/build.bat
@@ -1,11 +1,15 @@
 @echo off
+call :DetectJavaPath   JAVA_HOME
 
-set JAVA_HOME=D:\Program Files\Java\jdk-12.0.2
 set PATH=%PATH%;%JAVA_HOME%\bin;
 set CLASSHOME=%JAVA_HOME%\lib;%JAVA_HOME%\jre\lib\jrt-fs.jar;.
 
 set base_dir=
-goto:bat_main
+
+goto :bat_main
+
+pause
+goto :eof
 
 :set_all_java
     setlocal ENABLEDELAYEDEXPANSION
@@ -19,7 +23,7 @@ goto:bat_main
     echo srcs1 : %srcs1%
     echo srcs2 : !srcs2!
     endlocal & set "%~1=%srcs2%"
-goto:eof
+goto :eof
 
 :execute_java
     set all_java=%*
@@ -37,23 +41,102 @@ goto:eof
         )
     )
     endlocal
-goto:eof
+goto :eof
 
 :bat_main
     set base_dir=pic
-    call:color_text 4e "+++++++++++++++start+++++++++++++"
-    call:set_all_java m_srcs
+    call :color_text 4e "+++++++++++++++ bat_main +++++++++++++"
+    call :set_all_java  m_srcs
     
-    call:color_text 19 "+++++++++++++++compile srcs+++++++++++++"
-    call:compile_java %m_srcs%
+    call :compile_java %m_srcs%
  
-    call:color_text 2F "---------------execute srcs-------------"
     javac -encoding utf-8 -h . test\MakeAnimatedGifEncoder.java
     move test\*.class mypkg\
     @rem xcopy /s/e res mypkg\
     xcopy /e/h/q/y res mypkg\
     java mypkg.MakeAnimatedGifEncoder
-goto:eof
+    call :color_text 2F " -------------- bat_main ------------- "
+goto :eof
 
+:DetectJavaPath
+    setlocal EnableDelayedExpansion
 
+    call :color_text 2f " ++++++++++++++++++ DetectJavaPath +++++++++++++++++++++++ "
+    set VSDiskSet=C;D;E;F;G;
 
+    set AllProgramsPathSet="program"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"programs"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files (x86)"
+
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-1.8"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-1.8.0_60"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-12.0.2"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-23"
+
+    set idx_a=0
+    for %%A in (!VSDiskSet!) do (
+        set /a idx_a+=1
+        set idx_b=0
+        for %%B in (!AllProgramsPathSet!) do (
+            set /a idx_b+=1
+            set idx_c=0
+            for %%C in (!JavaPathSet!) do (
+                set /a idx_c+=1
+                set CurJavaDirName=%%A:\%%~B\%%~C\
+                echo [!idx_a!][!idx_b!][!idx_c!] !CurJavaDirName!
+                if exist !CurJavaDirName! (
+                    set JavaDirName=!CurJavaDirName!
+                    goto :DetectJavaPathBreak
+                )
+            )
+        )
+    )
+    :DetectJavaPathBreak
+    echo Use:%JavaDirName%
+    set PATH=%PATH%;%JavaDirName%\bin;
+    java -version
+    call :color_text 2f " -------------------- DetectJavaPath ----------------------- "
+    endlocal & set "%~1=%JavaDirName%"
+goto :eof
+
+:BuildJavaProj
+    setlocal EnableDelayedExpansion
+    set ProjDir=%~1
+
+    call :color_text 2f " +++++++++++++++++++ BuildJavaProj +++++++++++++++++++ "
+
+    call :DetectJavaPath   JAVA_HOME
+    set PATH=%JAVA_HOME%\bin;%PATH%;
+	
+	pushd %ProjDir%
+        javac -encoding utf-8 -d . callc.java
+        javap -s com.durongze.jni.CallC
+        rem javah -jni com.durongze.jni.CallC
+        javac -encoding utf-8 -h . callc.java
+    popd
+
+    call :color_text 2f " ------------------- BuildJavaProj ------------------- "
+
+    endlocal
+goto :eof
+
+@rem YellowBackground    6f  ef
+@rem BlueBackground      9f  bf   3f
+@rem GreenBackground     af  2f
+@rem RedBackground       4f  cf
+@rem GreyBackground      7f  8f
+@rem PurpleBackground    5f
+
+:color_text
+    setlocal EnableDelayedExpansion
+    for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+        set "DEL=%%a"
+    )
+    echo off
+    <nul set /p ".=%DEL%" > "%~2"
+    findstr /v /a:%1 /R "^$" "%~2" nul
+    del "%~2" > nul 2>&1
+    endlocal
+    echo .
+goto :eof
--- java/build_java.bat
+++ java/build_java.bat
@@ -1,18 +1,18 @@
 @echo off
+call :DetectJavaPath   JAVA_HOME
 
 rem jar -tvf lib/selenium-server-4.4.0.jar
 
-rem set JAVA_HOME=D:\Programs\jdk-13
-rem set CLASSPATH=%JAVA_HOME%\lib;%JAVA_HOME%\lib\jrt-fs.jar;.
-rem set PATH=%PATH%;%JAVA_HOME%\bin
-set JAVA_HOME=D:\Program Files\Java\jdk-12.0.2
 set PATH=%PATH%;%JAVA_HOME%\bin;
 set CLASSPATH=%JAVA_HOME%\lib;%JAVA_HOME%\jre\lib\jrt-fs.jar;.
 
 set base_dir=pic\
 set pkg_name=mypkg
 
-goto:bat_start
+goto :bat_start
+
+pause
+goto :eof
 
 :set_all_class
     setlocal ENABLEDELAYEDEXPANSION
@@ -24,7 +24,7 @@ goto:bat_start
     )
     echo objs : %objs1%
     endlocal & set "%~1=%objs1%"
-goto:eof
+goto :eof
 
 :set_all_java
     setlocal ENABLEDELAYEDEXPANSION
@@ -38,7 +38,7 @@ goto:eof
     echo srcs1 : %srcs1%
     echo srcs2 : !srcs2!
     endlocal & set "%~1=%srcs2%"
-goto:eof
+goto :eof
 
 :move_class
     set src_file=%1
@@ -49,52 +49,62 @@ goto:eof
         move !obj_file! %pkg_name%
     )
     endlocal
-goto:eof
+goto :eof
 
 :compile_
     set src_file=%*
     echo src_file : %src_file%
     javac -encoding utf-8 -d . %src_file%
-goto:eof
+goto :eof
 
 :compile_java
     set srcs=%*
+    call :color_text 2F " +++++++++++++++ compile_java +++++++++++++++ "
+    set idx=0
     for %%i in (%srcs%) do (
-        call:compile_ %%i
+        set /a idx+=1
+        set src_file=%%i
+        echo [!idx!] src_file: !src_file!
+        call :compile_ %%i
         if not %errorlevel% == 0 (
-            echo javac -encoding utf-8 %%i fail.................................
-            goto:eof
+            echo [!idx!] javac -encoding utf-8 %%i fail.................................
+            goto :eof
         ) else (
-            call:move_class %%i
+            call :move_class %%i
         )
     )
-goto:eof
+    call :color_text 2F " --------------- compile_java --------------- "
+goto :eof
 
 :execute_java
     set srcs=%*
     setlocal ENABLEDELAYEDEXPANSION
+    call :color_text 2F " +++++++++++++++ execute_java +++++++++++++++ "
+    set idx=0
     for %%i in (%srcs%) do (
+        set /a idx+=1
         set src_file=%%i
-        echo src_file: !src_file!
+        echo [!idx!] src_file: !src_file!
         set obj_name=!src_file:~0,-5!
         set obj_=!obj_name:%cd%\%base_dir%=!
         set pkg_=!obj_:\=.!
         java %pkg_name%.!pkg_!
         if not %errorlevel% == 0 (
-            echo java %pkg_name%.!pkg_! fail.................................
-            goto:eof
+            echo [!idx!] java %pkg_name%.!pkg_! fail.................................
+            goto :eof
         ) 
     )
+    call :color_text 2F " --------------- execute_java --------------- "
     endlocal
-goto:eof
+goto :eof
 
 :set_all_file
     @rem del mypkg\*.class
-    call:set_all_java m_srcs
-    echo m_srcs %m_srcs%
-    call:set_all_class m_objs
-    echo m_objs %m_objs%
-goto:eof
+    call :set_all_java  m_srcs
+    echo m_srcs=%m_srcs%
+    call :set_all_class m_objs
+    echo m_objs=%m_objs%
+goto :eof
 
 :color_text
     setlocal EnableDelayedExpansion
@@ -122,34 +132,98 @@ goto :eof
 goto :eof
 
 :bat_main
-    set base_dir=%1
-    call:color_text 4e "+++++++++++++++start+++++++++++++"
-    call:set_all_file
-
-    call:color_text 19 "+++++++++++++++compile srcs+++++++++++++"
-    call:compile_java %m_srcs%
- 
-    call:color_text 2F "---------------execute srcs-------------"
-    call:execute_java %m_srcs% 
+    setlocal EnableDelayedExpansion
+    set base_dir=%~1
+    call :color_text 2F " +++++++++++++++ bat_main +++++++++++++++ "
+    call :set_all_file
+    call :compile_java %m_srcs%
+    call :execute_java %m_srcs% 
+    call :color_text 2F " --------------- bat_main --------------- "
+    endlocal
 goto :eof
 
 :bat_test
+    setlocal EnableDelayedExpansion
     pushd test\
-    call build_email.bat
-    call build_excel.bat
-    call build_file.bat
-    call build_web.bat
+        call build_email.bat
+        call build_excel.bat
+        call build_file.bat
+        call build_web.bat
     popd
+    endlocal
 goto :eof
 
 :bat_start
-    call:bat_main pic\
+    setlocal EnableDelayedExpansion
+    call :bat_main pic\
     @rem call:bat_main test\
-    call:color_text 2F "---------------class_path-------------"
-    call:set_classpath CLASSPATH
+    call :color_text 2F " +++++++++++++++ bat_start +++++++++++++++ "
+    call :set_classpath CLASSPATH
     echo CLASSPATH:%class_path%
-    call:bat_main test\
+    call :bat_main test\
+    call :color_text 2F " --------------- bat_start --------------- "
+    endlocal
 goto :eof
 
-:bat_end
-    pause
+:DetectJavaPath
+    setlocal EnableDelayedExpansion
+
+    call :color_text 2f " ++++++++++++++++++ DetectJavaPath +++++++++++++++++++++++ "
+    set VSDiskSet=C;D;E;F;G;
+
+    set AllProgramsPathSet="program"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"programs"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files"
+    set AllProgramsPathSet=%AllProgramsPathSet%;"Program Files (x86)"
+
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-1.8"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-1.8.0_60"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-12.0.2"
+    set JavaPathSet=%JavaPathSet%;"Java\jdk-23"
+
+    set idx_a=0
+    for %%A in (!VSDiskSet!) do (
+        set /a idx_a+=1
+        set idx_b=0
+        for %%B in (!AllProgramsPathSet!) do (
+            set /a idx_b+=1
+            set idx_c=0
+            for %%C in (!JavaPathSet!) do (
+                set /a idx_c+=1
+                set CurJavaDirName=%%A:\%%~B\%%~C\
+                echo [!idx_a!][!idx_b!][!idx_c!] !CurJavaDirName!
+                if exist !CurJavaDirName! (
+                    set JavaDirName=!CurJavaDirName!
+                    goto :DetectJavaPathBreak
+                )
+            )
+        )
+    )
+    :DetectJavaPathBreak
+    echo Use:%JavaDirName%
+    set PATH=%PATH%;%JavaDirName%\bin;
+    java -version
+    call :color_text 2f " -------------------- DetectJavaPath ----------------------- "
+    endlocal & set "%~1=%JavaDirName%"
+goto :eof
+
+:BuildJavaProj
+    setlocal EnableDelayedExpansion
+    set ProjDir=%~1
+
+    call :color_text 2f " +++++++++++++++++++ BuildJavaProj +++++++++++++++++++ "
+
+    call :DetectJavaPath   JAVA_HOME
+    set PATH=%JAVA_HOME%\bin;%PATH%;
+	
+	pushd %ProjDir%
+        javac -encoding utf-8 -d . callc.java
+        javap -s com.durongze.jni.CallC
+        rem javah -jni com.durongze.jni.CallC
+        javac -encoding utf-8 -h . callc.java
+    popd
+
+    call :color_text 2f " ------------------- BuildJavaProj ------------------- "
+
+    endlocal
+goto :eof
